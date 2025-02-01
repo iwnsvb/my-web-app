d90cbd6dbda459de0e7d424da741f54b61573d02
iwnsvb <iwnsvb@gmail.com>
2025-02-01 21:18:01 +0800

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Ubuntu! 这是一个简单的网页应用。"
+    return render_template('index.html')  # 渲染 HTML 文件
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
